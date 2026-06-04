d9e2cb6e505c4368c4227f469e3928fe9925df75
Vinyas K S <vinyas.k.s.v26@gmail.com>
2026-06-04 12:52:15 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv("data_season.csv")
-
 st.title("📊 Dashboard")
+
+df = pd.read_csv("data_season.csv")
 
 col1, col2, col3, col4 = st.columns(4)
 
@@ -11,3 +11,5 @@ col1.metric("Total Records", len(df))
 col2.metric("Total Crops", df["Crops"].nunique())
 col3.metric("Locations", df["Location"].nunique())
 col4.metric("Average Yield", int(df["yeilds"].mean()))
+
+st.dataframe(df.head())
