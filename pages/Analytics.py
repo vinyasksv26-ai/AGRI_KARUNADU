4a32f530b9f2d2d24a4ffeb856c4e9e1665b6658
Vinyas K S <vinyas.k.s.v26@gmail.com>
2026-06-04 12:52:40 +0530

    Update Analytics.py

--- pages/Analytics.py
+++ pages/Analytics.py
@@ -2,17 +2,28 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv("data_season.csv")
-
 st.title("📈 Analytics")
+
+df = pd.read_csv("data_season.csv")
 
 crop_yield = df.groupby("Crops")["yeilds"].mean().reset_index()
 
-fig = px.bar(
+fig1 = px.bar(
     crop_yield,
     x="Crops",
     y="yeilds",
     title="Average Yield by Crop"
 )
 
-st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig1, use_container_width=True)
+
+season_yield = df.groupby("Season")["yeilds"].mean().reset_index()
+
+fig2 = px.pie(
+    season_yield,
+    names="Season",
+    values="yeilds",
+    title="Yield by Season"
+)
+
+st.plotly_chart(fig2, use_container_width=True)
