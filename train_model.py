c98ed2305c042386c0f60253877d8155e0d026b3
Vinyas K S <vinyas.k.s.v26@gmail.com>
2026-06-04 11:47:24 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
 import joblib
 
 df = pd.read_csv("data_season.csv")
@@ -11,7 +11,7 @@ df = pd.read_csv("data_season.csv")
 X = df.drop("yeilds", axis=1)
 y = df["yeilds"]
 
-categorical = [
+categorical_cols = [
     "Location",
     "Soil type",
     "Irrigation",
@@ -21,22 +21,32 @@ categorical = [
 
 preprocessor = ColumnTransformer(
     transformers=[
-        ("cat", OneHotEncoder(handle_unknown="ignore"), categorical)
+        (
+            "cat",
+            OneHotEncoder(handle_unknown="ignore"),
+            categorical_cols
+        )
     ],
     remainder="passthrough"
 )
 
 model = Pipeline([
     ("preprocessor", preprocessor),
-    ("regressor", RandomForestRegressor())
+    ("regressor", RandomForestRegressor(
+        n_estimators=100,
+        random_state=42
+    ))
 ])
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
 )
 
 model.fit(X_train, y_train)
 
 joblib.dump(model, "model.pkl")
 
-print("Model Saved")
+print("Model Saved Successfully")
